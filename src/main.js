6221d2cf67d0eb0d7c4592401530cde2acb4aeac
Negar <n.jamalifard@gmail.com>
2018-08-09 16:36:56 +0430

    css and icon packaged installed

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// Configuring FontAwesom component
+library.add(faCoffee);
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 new Vue({
   el: '#app',
+  components: {
+    'font-awesome-icon': FontAwesomeIcon
+  },
   render: h => h(App)
 })
