414d2f853ea347e5d1be2c7fb0ac11ea0fcb9739
Negar <n.jamalifard@gmail.com>
2018-08-09 23:13:38 +0430

    new task with a unique id could be added to the list

--- src/components/List.vue
+++ src/components/List.vue
@@ -4,14 +4,24 @@
     <div class="columns">
       <div class="column is-three-fifths is-offset-one-fifth">
 
-        <div class="field is-horizontal has-addons">
-          <div class="control is-expanded">
-            <input class="input is-medium" type="text" placeholder="Normal loading input">
+        <form @submit.prevent="createNewTask()">
+
+          <div class="field is-horizontal has-addons">
+            <div class="control is-expanded">
+
+              <input class="input is-medium"
+                     type="text"
+                     placeholder="Type your task title..."
+                     v-model="newTaskTitle"
+              >
+            </div>
+            <div class="control">
+              <button class="button is-medium is-primary" type="submit">Create Task</button>
+            </div>
           </div>
-          <div class="control">
-            <button class="button is-medium is-primary">Create task</button>
-          </div>
-        </div>
+
+        </form>
+
       </div>
     </div>
 
@@ -21,11 +31,8 @@
 
       <div class="column is-three-fifths is-offset-one-fifth">
         <ul>
-          <li>
-            <n-task-card></n-task-card>
-            <n-task-card></n-task-card>
-            <n-task-card></n-task-card>
-            <n-task-card></n-task-card>
+          <li v-for="task in tasks">
+            <n-task-card :id="task.id" :title="task.title" :deadline="task.deadline"></n-task-card>
           </li>
         </ul>
       </div>
@@ -39,10 +46,48 @@
 
 <script>
   import TaskCard from './TaskCard.vue';
+  // let randomId = require('../functions/randomId');
+  import { randomId } from '../functions/randomId';
+
 	export default {
 		name: "List",
     components: {
 		  "n-task-card": TaskCard
+    },
+    data(){
+		  return{
+		    tasks: [
+          {
+            id: "bala1",
+            title: "Task 1"
+          },
+          {
+            id: "bala2",
+            title: "Task 2",
+            deadline: "2018-08-10"
+          },
+          {
+            id: "bala3",
+            title: "Task 2"
+          },
+        ],
+        newTaskTitle: ""
+      }
+    },
+    // created(){
+    //   console.log(typeof randomId);
+    // },
+    methods:{
+      createNewTask(){
+        if(!this.newTaskTitle.length){
+          return;
+        }
+        this.tasks.unshift({
+          title: this.newTaskTitle,
+          id: randomId()
+        });
+        this.newTaskTitle = "";
+      }
     }
 
 	}
--- src/components/TaskCard.vue
+++ src/components/TaskCard.vue
@@ -1,34 +1,54 @@
 <template>
-  <div class="card">
+  <div class="card" :data-task-id="id" :key="id">
     <header class="card-header">
-      <p class="card-header-title">
-        Component
-      </p>
-      <a href="#" class="card-header-icon" aria-label="more options">
+      <div class="card-header-icon">
+        <span>
+          Status:
+        </span>
       <span class="icon">
+        <font-awesome-icon icon="coffee" />
         <i class="fas fa-angle-down" aria-hidden="true"></i>
       </span>
-      </a>
+      </div>
     </header>
     <div class="card-content">
       <div class="content">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
-        <a href="#">@bulmaio</a>. <a href="#">#css</a> <a href="#">#responsive</a>
-        <br>
-        <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+        <p class="title">
+          {{ title }}
+        </p>
+        <b>Task Deadline:</b>
+        <time datetime="2016-1-1">{{ deadline }}</time>
       </div>
     </div>
     <footer class="card-footer">
-      <a href="#" class="card-footer-item">Save</a>
-      <a href="#" class="card-footer-item">Edit</a>
-      <a href="#" class="card-footer-item">Delete</a>
+      <a :href=" '#'+ id " class="card-footer-item">View</a>
     </footer>
   </div>
 </template>
 
 <script>
 	export default {
-		name: "TaskCard"
+		name: "TaskCard",
+    props: {
+		  title: {
+		    type: String,
+        required: true
+      },
+      deadline: {
+		    type: String,
+        default: "No Deadline specified."
+      },
+      id: {
+		    type: String,
+        required: true
+      }
+    },
+    data(){
+		  return{
+
+
+      }
+    }
 	}
 </script>
 
