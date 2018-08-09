29a4ed8b79d621a1ae1ed6b9e65bf02e4b1cb17d
Negar <n.jamalifard@gmail.com>
2018-08-09 17:35:07 +0430

    list overview created

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,10 +1,50 @@
 <template>
-	$END$
+	<div>
+    <!--New Task-->
+    <div class="columns">
+      <div class="column is-three-fifths is-offset-one-fifth">
+
+        <div class="field is-horizontal has-addons">
+          <div class="control is-expanded">
+            <input class="input is-medium" type="text" placeholder="Normal loading input">
+          </div>
+          <div class="control">
+            <button class="button is-medium is-primary">Create task</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Tasks List -->
+
+    <div class="columns">
+
+      <div class="column is-three-fifths is-offset-one-fifth">
+        <ul>
+          <li>
+            <n-task-card></n-task-card>
+            <n-task-card></n-task-card>
+            <n-task-card></n-task-card>
+            <n-task-card></n-task-card>
+          </li>
+        </ul>
+      </div>
+
+
+    </div>
+
+
+  </div>
 </template>
 
 <script>
+  import TaskCard from './TaskCard.vue';
 	export default {
-		name: "List"
+		name: "List",
+    components: {
+		  "n-task-card": TaskCard
+    }
+
 	}
 </script>
 
--- src/components/TaskCard.vue
+++ src/components/TaskCard.vue
@@ -1,5 +1,29 @@
 <template>
-	$END$
+  <div class="card">
+    <header class="card-header">
+      <p class="card-header-title">
+        Component
+      </p>
+      <a href="#" class="card-header-icon" aria-label="more options">
+      <span class="icon">
+        <i class="fas fa-angle-down" aria-hidden="true"></i>
+      </span>
+      </a>
+    </header>
+    <div class="card-content">
+      <div class="content">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+        <a href="#">@bulmaio</a>. <a href="#">#css</a> <a href="#">#responsive</a>
+        <br>
+        <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+      </div>
+    </div>
+    <footer class="card-footer">
+      <a href="#" class="card-footer-item">Save</a>
+      <a href="#" class="card-footer-item">Edit</a>
+      <a href="#" class="card-footer-item">Delete</a>
+    </footer>
+  </div>
 </template>
 
 <script>
@@ -9,5 +33,7 @@
 </script>
 
 <style scoped>
-
+  .card{
+    margin-bottom: 20px;
+  }
 </style>
