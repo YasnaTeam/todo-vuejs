309b136e202042843833de703de43c04f4b5b119
Negar <n.jamalifard@gmail.com>
2018-08-09 17:04:40 +0430

    navbar created

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <template>
-
+    <div id="app">
+      <n-nav></n-nav>
+      <n-list-view></n-list-view>
+    </div>
 </template>
 
 <script>
+  import Nav from './components/Nav.vue';
+  import ListView from './components/ListView.vue';
+
 export default {
+  components : {
+    "n-nav": Nav,
+    "n-list-view": ListView
+  }
 
 }
 </script>
---
+++ src/components/Nav.vue
@@ -0,0 +1,23 @@
+<template>
+
+  <nav class="navbar has-background-light" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="https://bulma.io">
+        <img src="../assets/logo.png" alt="Vue Todo App">
+      </a>
+    </div>
+  </nav>
+
+</template>
+
+<script>
+	export default {
+		name: "Nav"
+	}
+</script>
+
+<style scoped>
+  .navbar{
+    box-shadow: 0 4px 20px rgba(0,0,0,0.18);
+  }
+</style>
